4ae4ae39de00bed23b73454cb7c51042b20e41c9
WaviestBalloon <edwardastill@hotmail.co.uk>
2022-05-26 11:07:10 +0100

    better code

--- serverbot.js
+++ serverbot.js
@@ -2,20 +2,52 @@ const Discord = require("discord.js");
 const sysinfo = require('systeminformation');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
+function sendandupdate(context) {
+	let temp = await sysinfo.cpuTemperature()
+	let cpu = await sysinfo.currentLoad()
+	let cpuspeed = await sysinfo.cpuCurrentSpeed()
+	let mem = await sysinfo.mem()
+
+	let embed = new Discord.MessageEmbed()
+		.setTitle("WavServer system information")
+		.setDescription(`CPU stress: ${Math.floor(cpu.currentLoad)}%\nCPU temperature: ${temp.main}°C\nCPU clock: ${cpuspeed.avg}GHz\nMemory: ${(mem.used / 1000000000).toPrecision(3)}GB/${(mem.total / 1000000000).toPrecision(3)}GB`)
+	let row = new MessageActionRow()
+		.addComponent(
+			new MessageButton()
+				.setLabel("Refresh")
+				.setStyle("PRIMARY")
+				.setCustomId("ref")
+				.setDisabled(false)
+	)
+
+	if (context.isInteraction()) {
+		interaction.message.edit({ embed: embed, components: [row] })
+	} else {
+		message.reply({ embed: embed, components: [row] })
+	}
+}
+
+
+client.on("interactionCreate", async (interaction) => {
+	if (interaction.isButton() && interaction.customId == "ref") {
+		let row = new MessageActionRow()
+			.addComponent(
+				new MessageButton()
+					.setLabel("Refreshing")
+					.setStyle("DANAGER")
+					.setCustomId("ref")
+					.setDisabled(true)
+		)
+		interaction.message.edit({ embeds: [new MessageEmbed({
+			title: "Fetching",
+		})], components: [row] })
+		sendandupdate(interaction)
+	}
+})
+
 client.on('message', async (message) => {
 	if (message.content == "<@978277851264581652>" || message.content == "<@!978277851264581652>" || message.content == "<@978277851264581652> ") {
-		let temp = await sysinfo.cpuTemperature()
-		let cpu = await sysinfo.currentLoad()
-		let cpuspeed = await sysinfo.cpuCurrentSpeed()
-		let mem = await sysinfo.mem()
-
-		console.log(temp)
-		console.log(cpuspeed)
-		let embed = new Discord.MessageEmbed()
-			.setTitle("WavServer system information")
-			.setDescription(`CPU stress: ${Math.floor(cpu.currentLoad)}%\nCPU temperature: ${temp.main}°C\nCPU clock: ${cpuspeed.avg}GHz\nMemory: ${mem.used / 1000000000}GB/${mem.total / 1000000000}GB`)
-
-		message.reply({ embeds: [embed] })
+		sendandupdate(message)
 	}
 })
 
