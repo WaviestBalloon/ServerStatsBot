9053d0580a59ad61264d7ba47540cab4b81346d6
WaviestBalloon <edwardastill@hotmail.co.uk>
2022-05-26 12:13:25 +0100

    looks better + ratio

--- serverbot.js
+++ serverbot.js
@@ -1,29 +1,43 @@
-const Discord = require("discord.js");
+const { MessageActionRow, MessageButton, MessageEmbed, Client } = require("discord.js");
 const sysinfo = require('systeminformation');
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-async function sendandupdate(context) {
+function bar(completed, total) {
+	let percent = Math.floor(completed / total * 15)
+	let bar = ""
+	for (let i = 0; i < percent; i++) {
+		bar += "█"
+	}
+	for (let i = percent; i < 15; i++) {
+		bar += "░"
+	}
+	return bar
+}
+
+async function sendandupdate(context, isinteraction) {
+	let begin = Date.now()
 	let temp = await sysinfo.cpuTemperature()
 	let cpu = await sysinfo.currentLoad()
 	let cpuspeed = await sysinfo.cpuCurrentSpeed()
 	let mem = await sysinfo.mem()
 
-	let embed = new Discord.MessageEmbed()
+	let embeddata = new MessageEmbed()
 		.setTitle("WavServer system information")
-		.setDescription(`CPU stress: ${Math.floor(cpu.currentLoad)}%\nCPU temperature: ${temp.main}°C\nCPU clock: ${cpuspeed.avg}GHz\nMemory: ${(mem.used / 1000000000).toPrecision(3)}GB/${(mem.total / 1000000000).toPrecision(3)}GB`)
+		.setDescription(`CPU stress: **${Math.floor(cpu.currentLoad)}%**\nCPU temperature: **${temp.main}°C**\nCPU clock: **${cpuspeed.avg}GHz**\nMemory: **${(mem.used / 1000000000).toPrecision(3)}GB**/**${(mem.total / 1000000000).toPrecision(3)}GB**\n\n\`CPU stress\`[${bar(cpu.currentLoad, 100)}]\n\`Memory utilisation\`[${bar(mem.used, mem.total)}]`)
 	let row = new MessageActionRow()
-		.addComponent(
+		.addComponents(
 			new MessageButton()
 				.setLabel("Refresh")
-				.setStyle("PRIMARY")
 				.setCustomId("ref")
 				.setDisabled(false)
-	)
-
-	if (context.isInteraction()) {
-		interaction.message.edit({ embed: embed, components: [row] })
+				.setStyle("PRIMARY"),
+		);
+	
+	if (isinteraction == true) {
+		context.message.edit({ embeds: [embeddata], components: [row] })
+		context.reply({ content: `Updated server stats in \`${Date.now() - begin}\` milliseconds.`, ephemeral: true })
 	} else {
-		message.reply({ embed: embed, components: [row] })
+		context.reply({ embeds: [embeddata], components: [row] })
 	}
 }
 
@@ -31,23 +45,22 @@ async function sendandupdate(context) {
 client.on("interactionCreate", async (interaction) => {
 	if (interaction.isButton() && interaction.customId == "ref") {
 		let row = new MessageActionRow()
-			.addComponent(
+			.addComponents(
 				new MessageButton()
-					.setLabel("Refreshing")
-					.setStyle("DANAGER")
+					.setLabel("Refresh")
 					.setCustomId("ref")
 					.setDisabled(true)
-		)
-		interaction.message.edit({ embeds: [new MessageEmbed({
-			title: "Fetching",
-		})], components: [row] })
-		sendandupdate(interaction)
+					.setStyle("DANGER"),
+			);
+		//interaction.deferReply()
+		interaction.message.edit({ embeds: [new MessageEmbed().setDescription("Fetching system data")], components: [row] })
+		sendandupdate(interaction, true)
 	}
 })
 
 client.on('message', async (message) => {
 	if (message.content == "<@978277851264581652>" || message.content == "<@!978277851264581652>" || message.content == "<@978277851264581652> ") {
-		sendandupdate(message)
+		sendandupdate(message, false)
 	}
 })
 
